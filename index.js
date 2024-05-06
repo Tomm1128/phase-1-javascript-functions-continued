870f68c687aae399b476c62054e2bb10c52df132
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-06 13:34:17 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function saturdayFun(activity){
+    // This functio returns a string with the activity that is passed into the function
+    
+    if (typeof activity != "undefined"){
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+
+const mondayWork = function(activity){
+    if (typeof activity != "undefined"){
+        return `This Monday, I will ${activity}.`;
+    } else {
+        return `This Monday, I will go to the office.`;
+    }
+}
+
+function wrapAdjective(special){
+     return function(flair){
+        if (typeof special != "undefined"){
+            return `You are ${special}${flair}${special}!`;
+        } else {
+            special = "*";
+            return `You are ${special}${flair}${special}!`;
+        }
+    }
+}
